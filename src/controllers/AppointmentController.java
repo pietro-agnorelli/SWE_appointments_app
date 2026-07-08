ccd394a4e00e2e3833e7d6d2f4121b172edcd320
Pietro Agnorelli <pietro.agnorelli@edu.unifi.it>
2026-07-08 10:54:30 +0200

    Finished adding controllers

---
+++ src/controllers/AppointmentController.java
@@ -0,0 +1,74 @@
+package controllers;
+
+import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import utilities.AppSession; 
+import model.Appointment;
+import model.Client;
+import model.User;
+import database.dao.AppointmentDao;
+import views.AppointmentView;
+import views.ClientView;
+import views.CommonView;
+
+public class AppointmentController {
+	CommonView commonView = new CommonView();
+	AppointmentView appointmentView = new AppointmentView();
+	ClientView clientView = new ClientView();
+	AppointmentDao appointmentDao = new AppointmentDao();
+	
+	public void viewUserAppointments(){
+		User currentUser = AppSession.getInstance().getCurrentUser();
+		List<Appointment> appointments = appointmentDao.getAppointmentsByUserId(currentUser);
+		if (appointments.isEmpty()) {
+			commonView.displayMessage("No appointments found for user: " + currentUser.getUsername());
+		} else {
+			appointmentView.displayUserAppointments(appointments);
+		}
+	}
+	
+	public void manageAppointments() {
+		while (true) {
+			int choice = appointmentView.showAppointmentMenu();
+			switch (choice) {
+				case 1:
+					viewClientAppointments();
+					continue;
+				case 2:
+					addAppointment();
+					continue;
+				case 3:
+					return; // Exit to main menu
+				default:
+					commonView.displayMessage("Invalid choice. Please try again.");
+			}
+		}
+	}
+	
+	public void viewClientAppointments() {
+		Client currentClient = AppSession.getInstance().getCurrentClient();
+		List<Appointment> appointments = appointmentDao.getAppointmentsByClientId(currentClient);
+		if (appointments.isEmpty()) {
+			commonView.displayMessage("No appointments found for client: " + currentClient.getName());
+		} else {
+			appointmentView.displayClientAppointments(appointments);
+		}
+	}
+	
+	public void addAppointment() {
+		String date = appointmentView.askForDate();
+		String time = appointmentView.askForTime();
+		Appointment newAppointment = new Appointment(AppSession.getInstance().getCurrentUser().getId(), 
+				AppSession.getInstance().getCurrentClient().getId(), 
+				LocalDate.parse(date), 
+				LocalTime.parse(time));
+		appointmentDao.addAppointment(newAppointment);
+		commonView.displayMessage("Appointment added successfully.");
+		}
+		
+}
+	
+	
+
---
+++ src/controllers/TreatmentController.java
@@ -0,0 +1,61 @@
+package controllers;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import views.TreatmentView;
+import views.CommonView;
+import model.Client;
+import model.Treatment;
+import utilities.AppSession;
+import database.dao.TreatmentDao;
+
+
+public class TreatmentController {
+	TreatmentView treatmentView = new TreatmentView();
+	TreatmentDao treatmentDao = new TreatmentDao();
+	CommonView commonView = new CommonView();
+	
+	public void manageTreatments() {
+		while (true) {
+			
+			int choice = treatmentView.showTreatmentMenu();
+			
+			switch (choice) {
+				case 1:
+					addTreatment();
+					continue;
+				case 2:
+					viewTreatments();
+					continue;
+				case 3:
+					return; // Exit the treatment management
+				default:
+					System.out.println("Invalid choice. Please try again.");
+			}
+		}
+	}
+	
+	public void addTreatment() {
+		String treatmentDescription = treatmentView.askForDescription();
+		String date = treatmentView.askForDate();
+		Treatment newTreatment = new Treatment(AppSession.getInstance().getCurrentUser().getId(),
+				AppSession.getInstance().getCurrentClient().getId(),
+				LocalDate.parse(date),
+				treatmentDescription);
+		treatmentDao.addTreatment(newTreatment);
+		commonView.displayMessage("Treatment added successfully.");
+	}
+	
+	public void viewTreatments() {
+		Client currentClient = AppSession.getInstance().getCurrentClient();
+		List<Treatment> treatments = treatmentDao.getTreatmentsByClientId(currentClient);
+		if (treatments.isEmpty()) {
+			commonView.displayMessage("No treatments found for client: " + currentClient.getName());
+		} else {
+			treatmentView.displayTreatments(treatments);
+		}
+		
+	}
+
+}
