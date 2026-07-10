133418bf38a05854c49fd9dcfbb817e6898d3a7c
Pietro Agnorelli <pietro.agnorelli@edu.unifi.it>
2026-07-10 15:45:53 +0200

    Added model tests

---
+++ src/test/model/AppointmentTest.java
@@ -0,0 +1,45 @@
+package test.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.Test;
+
+import model.Appointment;
+
+class AppointmentTest {
+
+	@Test
+	void testConstructorsAndGetters() {
+		LocalDate date = LocalDate.parse("2020-01-01");
+		LocalDate date1 = LocalDate.parse("2020-01-02");
+		LocalTime time = LocalTime.parse("09:00");
+		LocalTime time1 = LocalTime.parse("10:00");
+		Appointment appointment = new Appointment(1, 1, date, time);
+		Appointment appointment1 = new Appointment(1, 2, 2, date1, time1);
+		long id = appointment.getId();
+		long id1 = appointment1.getId();
+		long userId = appointment.getUserId();
+		long userId1 = appointment1.getUserId();
+		long clientId = appointment.getClientId();
+		long clientId1 = appointment1.getClientId();
+		LocalDate dateTest = appointment.getDate();
+		LocalDate dateTest1 = appointment1.getDate();
+		LocalTime timeTest = appointment.getStartTime();
+		LocalTime timeTest1 = appointment1.getStartTime();
+		
+		assertEquals(id, 0);
+		assertEquals(id1, 1);
+		assertEquals(userId, 1);
+		assertEquals(userId1, 2);
+		assertEquals(clientId, 1);
+		assertEquals(clientId1, 2);
+		assertEquals(dateTest, date);
+		assertEquals(dateTest1, date1);
+		assertEquals(timeTest, time);
+		assertEquals(timeTest1, time1);
+	}
+
+}
---
+++ src/test/model/UserTest.java
@@ -0,0 +1,27 @@
+package test.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import model.User;
+
+class UserTest {
+
+	@Test
+	void testConstructorsAndGetters() {
+		User user = new User("test");
+		User user1 = new User(1, "test1");
+		String name = user.getUsername();
+		String name1 = user1.getUsername();
+		long id = user.getId();
+		long id1 = user1.getId();
+		
+		
+		assertEquals(name, "test");
+		assertEquals(name1, "test1");
+		assertEquals(id, 0);
+		assertEquals(id1, 1);
+	}
+
+}
