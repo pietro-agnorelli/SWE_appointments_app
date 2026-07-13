5e2b693a757b632e7329af6f7800fb6314c5795f
Pietro Agnorelli <pietro.agnorelli@edu.unifi.it>
2026-07-13 10:23:43 +0200

    Adjusted connection test with previous changes

--- src/test/database/DBConnectionTest.java
+++ src/test/database/DBConnectionTest.java
@@ -13,11 +13,7 @@ public class DBConnectionTest {
 	
 	@Test
 	void test() {
-		try {
 			Connection connection = DBConnection.getConnection();
 			assertNotNull(connection);
-		} catch (SQLException e) {
-			fail(e.getMessage());
-		}
 	}
 }
